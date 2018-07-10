7108ab152fdccc1eebf3241d61d85154bc88a8ba
skkin <970110leo@gmail.com>
2018-07-10 23:27:03 +0800

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <v-title title="Vue组件化"></v-title>
+    <v-button @click="handleClick">点击按钮</v-button>
+    <p>
+      <img src="./images/image.jpg" style="width: 200px">
+    </p>
+  </div>
+</template>
+<script>
+import vTitle from './title.vue';
+import vButton from './button.vue';
+export default {
+  components: {
+    vTitle,
+    vButton
+  },
+  methods: {
+    handleClick(e) {
+      console.log(e)
+    }
+  },
+  data() {
+    return {
+      name: 'Vue.js'
+    }
+  }
+}
+</script>
+<style scoped>
+  div {
+    color: #f60;
+    font-size: 24px;
+  }
+</style>
+
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var path = require('path')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var VueLoaderPlugin = require('vue-loader/lib/plugin')
+var config = {
+  entry: {
+    main: './main'
+  },
+  output: {
+    path: path.join(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            css: ExtractTextPlugin.extract({
+              use: 'css-loader',
+              fallback: 'vue-style-loader'
+            })
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+        use: [
+          {
+            loader: 'url-loader?limit=1024'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    //重命名提取后的css文件
+    new ExtractTextPlugin("main.css"),
+    new VueLoaderPlugin()
+  ]
+};
+module.exports = config;
